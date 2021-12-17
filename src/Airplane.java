e11aa4533ad74e73350bcd4d7c600249f3a67036
SentientPlatypus <trexxxxxxxxxxy@gmail.com>
2021-12-17 16:41:07 -0500

    Made code flexible for FirstClass, EconomyClass and allseats

--- src/Airplane.java
+++ src/Airplane.java
@@ -4,7 +4,9 @@ public class Airplane {
     private Seat[][] FirstClassSeating = new Seat[5][4];
     private Seat[][] EconomyClassSeating = new Seat[35][6];
 
-    private Seat[][] AllSeats;
+    public static byte ALLSEATS = 0;
+    public static byte FIRSTCLASS=1;
+    public static byte ECONOMYCLASS=2;
 
     public Airplane()
     {
@@ -40,22 +42,34 @@ public class Airplane {
         return FirstClassSeating;
     }
 
-    public Seat getSeat(int row, int column)
-    {
-        row-=1;
-        column-=1;
-        if (row >= 5)
-        {
-            row -=5;
 
+    public Seat getSeat(int row, int column, byte seat)
+    {
+        assert seat == ALLSEATS || seat == FIRSTCLASS || seat == ECONOMYCLASS;
+        row -= 1;
+        column -= 1;
+        if (seat == ALLSEATS)
+        {
+            if (row >= 5) {
+                row -= 5;
+
+                return EconomyClassSeating[row][column];
+            }
+            return FirstClassSeating[row][column];
+        }
+        else if (seat == FIRSTCLASS)
+        {
+            return FirstClassSeating[row][column];
+        }
+        else
+        {
             return EconomyClassSeating[row][column];
         }
-        return FirstClassSeating[row][column];
     }
 
-    public String getSeatingLayout()
+    public String getFirstClassLayout()
     {
-        String layout = "    1  2  3  4  5  6\n";
+        String layout = "    0  1  2  3  4   \n";
         for (int i = 0; i < FirstClassSeating.length; i++)
         {
             if (i + 1 < 10)
@@ -67,6 +81,12 @@ public class Airplane {
                 layout += i + 1 + "    " + Arrays.deepToString(FirstClassSeating[i]) + "\n";
             }
         }
+        return layout;
+    }
+
+    public String getEconomyClassLayout()
+    {
+        String layout = "    1  2  3  4  5  6\n";
         for (int i = 0; i < EconomyClassSeating.length; i++)
         {
             if (i + 1 < 10)
@@ -79,5 +99,61 @@ public class Airplane {
             }
         }
         return layout;
+    }
+
+    public String getSeatingLayout(byte seatSelection)
+    {
+        assert  seatSelection == ALLSEATS || seatSelection == ECONOMYCLASS || seatSelection == FIRSTCLASS;
+        if (seatSelection == ALLSEATS)
+        {
+            String layout = "    1  2  3  4  5  6\n";
+            for (int i = 0; i < FirstClassSeating.length; i++) {
+                if (i + 1 < 10) {
+                    layout += i + 1 + "     " + Arrays.deepToString(FirstClassSeating[i]) + "\n";
+                } else {
+                    layout += i + 1 + "    " + Arrays.deepToString(FirstClassSeating[i]) + "\n";
+                }
+            }
+            for (int i = 0; i < EconomyClassSeating.length; i++) {
+                if (i + 1 < 10) {
+                    layout += 5 + i + "  " + Arrays.deepToString(EconomyClassSeating[i]) + "\n";
+                } else {
+                    layout += 5 + i + " " + Arrays.deepToString(EconomyClassSeating[i]) + "\n";
+                }
+            }
+            return layout;
+        }
+        else if (seatSelection == ECONOMYCLASS)
+        {
+            String layout = "    1  2  3  4  5  6\n";
+            for (int i = 0; i < EconomyClassSeating.length; i++)
+            {
+                if (i + 1 < 10)
+                {
+                    layout += i + 1 + "  " + Arrays.deepToString(EconomyClassSeating[i]) + "\n";
+                }
+                else
+                {
+                    layout += i + 1 + " " + Arrays.deepToString(EconomyClassSeating[i]) + "\n";
+                }
+            }
+            return layout;
+        }
+        else
+        {
+            String layout = "    0  1  2  3  4   \n";
+            for (int i = 0; i < FirstClassSeating.length; i++)
+            {
+                if (i + 1 < 10)
+                {
+                    layout += i + 1 + "     " + Arrays.deepToString(FirstClassSeating[i]) +"\n";
+                }
+                else
+                {
+                    layout += i + 1 + "    " + Arrays.deepToString(FirstClassSeating[i]) + "\n";
+                }
+            }
+            return layout;
+        }
     }
 }
--- src/tester.java
+++ src/tester.java
@@ -6,11 +6,11 @@ public class tester
     {
         System.out.println("hi");
         Airplane plane = new Airplane();
-        System.out.println(plane.getSeatingLayout());
-        Seat sixFive = plane.getSeat(6, 5);
+        System.out.println(plane.getSeatingLayout(Airplane.FIRSTCLASS));
+        Seat sixFive = plane.getSeat(3, 4, Airplane.FIRSTCLASS);
         System.out.println(sixFive);
         sixFive.occupy();
         System.out.println(sixFive);
-        System.out.println(plane.getSeatingLayout());
+        System.out.println(plane.getSeatingLayout(Airplane.FIRSTCLASS));
     }
 }
