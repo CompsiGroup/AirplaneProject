cdb1e9c2690f593a38ba05af542adef0346a372b
SentientPlatypus <trexxxxxxxxxxy@gmail.com>
2021-12-17 09:16:06 -0500

    testing if occupy wrks

--- src/tester.java
+++ src/tester.java
@@ -7,6 +7,10 @@ public class tester
         System.out.println("hi");
         Airplane plane = new Airplane();
         System.out.println(plane.getSeatingLayout());
-        System.out.println(plane.getSeat(6, 5));
+        Seat sixFive = plane.getSeat(6, 5);
+        System.out.println(sixFive);
+        sixFive.occupy();
+        System.out.println(sixFive);
+        System.out.println(plane.getSeatingLayout());
     }
 }
